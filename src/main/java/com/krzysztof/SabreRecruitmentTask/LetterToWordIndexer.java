da92f09091af4272aa9450f9a50a74c560d30749
KrzysztofP90 <krzysztof1.przybylowicz@gmail.com>
2019-08-07 10:22:44 +0200

    Implement add keys to letter word map from words.

--- src/main/java/com/krzysztof/SabreRecruitmentTask/LetterToWordIndexer.java
+++ src/main/java/com/krzysztof/SabreRecruitmentTask/LetterToWordIndexer.java
@@ -1,5 +1,44 @@
 package com.krzysztof.SabreRecruitmentTask;
 
+import java.util.*;
+import java.util.stream.Stream;
+
 public class LetterToWordIndexer {
 
+    private Map<Character, List<String>> letterWordMap;
+
+    public LetterToWordIndexer(String text) {
+        this.letterWordMap = new TreeMap<>();
+        initialMap(text);
+    }
+
+    public Map<Character, List<String>> getLetterWordMap() {
+        return this.letterWordMap;
+    }
+
+    private void initialMap(String text) {
+        createLetterWordMapFromText(text);
+    }
+
+    private void createLetterWordMapFromText(String text) {
+        String[] arrayOfWord = this.getDistinctWordList(text);
+        addKeysToMapFromWordsArray(arrayOfWord);
+
+    }
+
+    private String[] getDistinctWordList(String text) {
+        return Arrays.stream(text.split(" ")).distinct().map(String::toLowerCase)
+                .toArray(String[]::new);
+    }
+
+    private void addKeysToMapFromWordsArray(String[] arrayOfWord) {
+        Arrays.stream(arrayOfWord).forEach(this::addKeysFromWord);
+    }
+
+    private void addKeysFromWord(String word) {
+        Stream<Character> charStream =  word.chars().distinct().mapToObj(ch -> (char) ch);
+        charStream.forEach(c -> {if (!letterWordMap.containsKey(c)) {
+            letterWordMap.put(c, new ArrayList<>());
+        }});
+    }
 }
--- src/test/java/com/krzysztof/SabreRecruitmentTask/LetterToWordIndexerTest.java
+++ src/test/java/com/krzysztof/SabreRecruitmentTask/LetterToWordIndexerTest.java
@@ -1,12 +1,27 @@
 package com.krzysztof.SabreRecruitmentTask;
-import static org.junit.Assert.assertTrue;
+import static junit.framework.TestCase.assertEquals;
+
 import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class LetterToWordIndexerTest {
 
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+     @Test
+    public void checkIfAllLettersAreInKeySet() {
+        String testInput = "Ala has a cat";
+        LetterToWordIndexer indexer = new LetterToWordIndexer(testInput);
+
+        Set<Character> expectedSetOfKeys = new HashSet<>();
+        expectedSetOfKeys.add('a');
+        expectedSetOfKeys.add('l');
+        expectedSetOfKeys.add('h');
+        expectedSetOfKeys.add('s');
+        expectedSetOfKeys.add('c');
+        expectedSetOfKeys.add('t');
+        Set<Character> actualSetOfKey = indexer.getLetterWordMap().keySet();
+
+        assertEquals(expectedSetOfKeys, actualSetOfKey);
     }
 }
